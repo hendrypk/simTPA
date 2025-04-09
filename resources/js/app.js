3770df3ca6a2fc438a4e6134ab16dac7c72dcbd1
“[Hendry <hendryputra934@gmail.com>
2025-04-09 16:53:09 +0700

    fix font js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
 import './bootstrap';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './sb-admin-2.min.js';
+import '../vendor/chart.js/Chart.min.js';
+
 
--- resources/views/admin/_layout/main.blade.php
+++ resources/views/admin/_layout/main.blade.php
@@ -174,8 +174,8 @@
     @vite([
         // 'resources/js/sb-admin-2.js',
         'resources/js/app.js',
-        'resources/js/sb-admin-2.min.js',
-        'resources/vendor/chart.js/Chart.min.js',
+        // 'resources/js/sb-admin-2.min.js',
+        // 'resources/vendor/chart.js/Chart.min.js',
     ])
     @yield('script')
 </body>
