9a25c10a67162c26ecf3ce336ec9bb70c84725c0
“[Hendry <hendryputra934@gmail.com>
2025-05-10 23:59:18 +0700

    fix transactions migration table

--- database/migrations/2025_03_26_074602_create_transactions_table.php
+++ database/migrations/2025_03_26_074602_create_transactions_table.php
@@ -11,20 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
-            $table->id();
-            $table->integer('transaction_id');
-            $table->unsignedBigInteger('contact_id');
-            $table->unsignedBigInteger('wallet_id');
-            $table->date('date');
-            $table->unsignedBigInteger('category_id');
-            $table->integer('amount');
-            $table->string('description');
-            $table->string('attachment');
-            $table->boolean('hide_contact')->nullable();
-            $table->softDeletes();
-            $table->timestamps();
-        });
+        // Schema::create('transactions', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->integer('transaction_id');
+        //     $table->unsignedBigInteger('contact_id');
+        //     $table->unsignedBigInteger('wallet_id');
+        //     $table->date('date');
+        //     $table->unsignedBigInteger('category_id');
+        //     $table->integer('amount');
+        //     $table->string('description');
+        //     $table->string('attachment');
+        //     $table->boolean('hide_contact')->nullable();
+        //     $table->softDeletes();
+        //     $table->timestamps();
+        // });
     }
 
     /**
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('transactions');
+        // Schema::dropIfExists('transactions');
     }
 };
