c6ea53e9fbb1fd1c155c91dbdd6e183c9783ff8c
“[Hendry <hendryputra934@gmail.com>
2025-05-10 16:13:56 +0700

    fix transactions migration

--- database/migrations/2025_04_18_141148_create_transactions_table.php
+++ database/migrations/2025_04_18_141148_create_transactions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::update('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('payable_id')->constrained('options');
             $table->foreignId('wallet_id')->constrained('options');
